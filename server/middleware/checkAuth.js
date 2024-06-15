69d9813e06289957f3ed8cdaf1f1880dafd3cfe0
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2024-06-15 20:59:15 +0300

    rollback

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,6 +1,5 @@
 module.exports = {
 	userAuthenticated: (req, res, next) => {
-		console.log(req.user, req.isAuthenticated())
 		if (req.isAuthenticated()) next()
 		else res.json({ error: 'unauthorized', loggedIn: false })
 	},
