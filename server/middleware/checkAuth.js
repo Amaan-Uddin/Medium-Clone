2a3635cfe13ea02f05f0c53580640a17fff5c54f
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2024-06-16 05:48:33 +0300

    changes to vercel.json

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,5 +1,6 @@
 module.exports = {
 	userAuthenticated: (req, res, next) => {
+		console.log(req.user, req.session, req.isAuthenticated())
 		if (req.isAuthenticated()) next()
 		else res.json({ error: 'unauthorized', loggedIn: false })
 	},
